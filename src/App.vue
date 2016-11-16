47780edb33d6b775da7fdad80f815418c2881ad8
kfei <kfei@kfei.net>
2016-11-16 22:47:40 +0800

    Now jQuery works
    Signed-off-by: kfei <kfei@kfei.net>

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,25 @@
   <div id="app">
     <img src="./assets/logo.png">
     <hello></hello>
+    <button v-on:click="changeColor">Click me</button>
   </div>
 </template>
 
 <script>
+import $ from 'jquery';
 import Hello from './components/Hello';
 
 export default {
   name: 'app',
   components: {
     Hello,
+  },
+  methods: {
+    changeColor: function changeColor() {
+      /* eslint-disable no-console */
+      // console.log($(this.$el));
+      $(this.$el).css('background-color', 'grey');
+    },
   },
 };
 </script>
