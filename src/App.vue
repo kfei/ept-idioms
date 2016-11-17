ed2d68406c3b9a392f283ed532737845b94e5b8a
kfei <kfei@kfei.net>
2016-11-17 19:06:07 +0800

    Revert all Onsen-UI related code
    Will try Onsen when it starts to support Vue 2.
    
    Signed-off-by: kfei <kfei@kfei.net>

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,25 @@
 <template>
   <div id="app">
-    <ons-page>
-      <ons-toolbar>
-        <div class="left">
-          <ons-toolbar-button>
-            <ons-icon icon="ion-navicon, material:md-menu"></ons-icon>
-          </ons-toolbar-button>
-        </div>
-        <div class="center">ept-idioms</div>
-      </ons-toolbar>
-      <img src="./assets/logo.png">
-      <index></index>
-      <p style="text-align: center">
-        <ons-button
-          @click="$notification.alert(message)"
-        >
-          Click me!
-        </ons-button>
-      </p>
-      <p style="height: 400px;">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A illum cumque placeat nostrum fuga ipsam eveniet ab incidunt perspiciatis laudantium, vel aut eligendi qui sed cum fugiat harum praesentium nesciunt.
-      </p>
-    </ons-page>
+    <img src="./assets/logo.png">
+    <hello></hello>
+    <button v-on:click="changeColor">Click me</button>
   </div>
 </template>
 
 <script>
-// import $ from 'jquery';
-import Index from './components/Index';
+import $ from 'jquery';
+import Hello from './components/Hello';
 
 export default {
   name: 'app',
   components: {
-    Index,
-  },
-  data() {
-    return {
-      message: 'Hello world!',
-    };
+    Hello,
   },
   methods: {
-    onChange: function onChange(event) {
+    changeColor: function changeColor() {
       /* eslint-disable no-console */
       // console.log($(this.$el));
-      this.switchState = event.target.checked;
+      $(this.$el).css('background-color', 'grey');
     },
   },
 };
--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
 import Vue from 'vue';
-import ons from 'onsenui';
-import VueOnsen from 'vue-onsenui';
 import App from './App';
 
-import '../node_modules/onsenui/css/onsenui.css';
-import '../node_modules/onsenui/css/onsen-css-components.css';
-
-Vue.use(VueOnsen);
-
-ons.ready(() => {
-  /* eslint-disable no-new */
-  new Vue({
-    el: '#app',
-    template: '<App/>',
-    components: { App },
-  });
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  template: '<App/>',
+  components: { App },
 });
